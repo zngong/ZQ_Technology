6c270e4ecf30bd6a5552797cdf678cbe7299272e
龚忠念 <840691810gzn@sina.com>
2017-12-20 19:21:04 +0800

    import HttpModule
    引入HttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,20 @@ import { NgModule } from '@angular/core';
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpModule }    from '@angular/http';
 import  { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    HttpModule,
     BrowserAnimationsModule,
     NgbModule.forRoot()
     
